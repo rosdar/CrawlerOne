f3b85c16e589653b4d281131526575e01ef9c039
roslavych <droslavych@gmail.com>
2025-05-21 16:58:36 +0200

    Add filters tests and feature. Add user interaction menu.

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,8 +14,24 @@ def load_html_empty_fixture() -> str:
     return _load_fixture("hackernews_empty_sample.html")
 
 
-def load_entries_fixture() -> list[Entry]:
-    data = _load_fixture("hackernews_entries.json")
+def load_all_entries_fixture() -> list[Entry]:
+    return _load_entries_fixture("hackernews_entries.json")
+
+
+def load_sample_entries_fixture() -> list[Entry]:
+    return _load_entries_fixture("sample_entries.json")
+
+
+def load_sample_entries_filter_long_comments_fixture() -> list[Entry]:
+    return _load_entries_fixture("sample_entries_filter_long_comments.json")
+
+
+def load_sample_entries_filter_short_points_fixture() -> list[Entry]:
+    return _load_entries_fixture("sample_entries_filter_short_points.json")
+
+
+def _load_entries_fixture(filename: str) -> list[Entry]:
+    data = _load_fixture(filename)
     entries_data = json.loads(data)
     return [Entry(**entry) for entry in entries_data]
 
