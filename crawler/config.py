7977b89816f5854599b9b6e26018b7aad4f7d6cf
roslavych <droslavych@gmail.com>
2025-05-21 13:21:55 +0200

    Add fetcher basic tests and functions.

---
+++ crawler/config.py
@@ -0,0 +1,1 @@
+BASE_URL = "https://news.ycombinator.com"
--- crawler/fetcher.py
+++ crawler/fetcher.py
@@ -1,21 +1,67 @@
-import asyncio
+import requests
+from bs4 import BeautifulSoup, NavigableString, Tag
+from requests import RequestException, Response
 
-import httpx
+from crawler.config import BASE_URL
+from crawler.models.entry import Entry
 
 
-async def fetch_url(url: str) -> str:
+def fetch_url(url: str) -> Response:
 
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
+    try:
+        response: Response = requests.get(url)
         response.raise_for_status()
-        print(response.text)
-        return str(response.text)
+        return response
+    except RequestException as e:
+        # Handle/log if needed
+        raise e
+
+
+def get_first_30_entries(content: str) -> list[Entry]:
+
+    soup = BeautifulSoup(content, 'html.parser')
+    entries: list[Entry] = []
+
+    # Extract ranks, titles, points, and comments
+    for item in soup.find_all('tr', class_='athing submission'):
+
+        rank_tag: Tag = item.find('span', class_='rank')
+        title_tag: Tag = item.find('span', class_='titleline').find_all('a')[0]
+        subtext: Tag = item.find_next_sibling('tr').find('td', class_='subtext')
+
+        if rank_tag and title_tag and subtext:
+
+            points_tag: Tag | NavigableString | None = subtext.find('span', class_='score')
+            comments_tag: Tag = subtext.find_all('a')[-1]
+
+            rank: int = int(rank_tag.text.strip('.'))
+            title: str = title_tag.text
+            points: int = int(points_tag.text.split()[0]) if points_tag else 0
+            number_comments = int(comments_tag.text.split()[0]) if comments_tag and 'comment' in comments_tag.text else 0
+
+            entries.append(Entry(rank=rank, title=title, points=points, number_comments=number_comments))
+
+    if len(entries) == 0:
+        raise ValueError("No entries found!")
+
+    return entries[:30]
 
 
 def main() -> None:
 
-    url = input("Enter a URL to fetch: ")
-    asyncio.run(fetch_url(url))
+    print(f"Fetching {BASE_URL}")
+
+    try:
+        content: Response = fetch_url(BASE_URL)
+    except RequestException as e:
+        print(f"Error fetching URL: {e}")
+        return
+
+    entries = get_first_30_entries(content.text)
+    if entries:
+        for entry in entries:
+            print(f"Entry {entry.rank}: \"{entry.title}\"")
+            print(f"{entry.points} points and {entry.number_comments} comments")
 
 
 if __name__ == "__main__":
